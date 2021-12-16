9b1150b188b69a984d6adf79b2d64f170d5e99e3
Artem Dmytrishyn <gamegir@gmail.com>
2021-12-16 16:03:01 +0300

    del meta og and twitter

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -36,34 +36,6 @@ const SEO = ({ description, lang, meta, title, keywords}) => {
           name: `keywords`,
           content: metaKeywords,
         },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata?.author || ``,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
       ].concat(meta)}
     />
   )
