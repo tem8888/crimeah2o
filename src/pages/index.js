e78fbb646e7a599cbe2d806781fef93ad5eb144c
Artem Dmytrishyn <gamegir@gmail.com>
2022-01-08 20:52:25 +0300

    upd

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Seo from "../components/Seo"
 export default function Home() {
   return (
     <Layout>
-      <Seo title="Единая доставка воды в Крыму | Доставка питьевой воды в Симферополе" url="/"/>
+      <Seo title="Единая доставка воды в Крыму | Доставка питьевой воды по Симферополю и Симферопольскому району" url="/"/>
       <HomeMenu />
       <section className="seo">
         <h3>Большой выбор и лучшие цены!</h3>
