7aa9d0440669f4e7c9e3b08e4b085a0d67f2a726
Artem Dmytrishyn <gamegir@gmail.com>
2022-01-15 15:02:22 +0300

    upd

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,15 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   /* Your site config here */
   siteMetadata: {
     siteUrl: `https://crimea-h2o.ru`,
-    title: "Единая доставка воды в Крыму",
+    title: `Единая доставка воды в Крыму`,
     description: `Доставка питьевой воды в Симферополе. Мы предлагаем вам большой ассортимент вкусной и качественной воды, которая прекрасно подходит, как для питья, 
     так и для приготовления пищи. Наша вода кристально чистая, а цена в Симферополе одна из самых низких. Заказ воды: г. Симферополь, тел. +7(978) 869-0005, +7(978) 869-0009. 
     Режим работы: пн-вс 08:00 - 18:00. Доставка бесплатная.`,
@@ -46,6 +49,31 @@ module.exports = {
         theme_color: "#a2466c",
         display: "standalone",
         icon: "src/images/favicon.png",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          process.env.GOOGLEID
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
       },
     },
   ],
