ab8ecdb62ae7a1160af8f587e79edc2672606cf3
Artem Dmytrishyn <gamegir@gmail.com>
2022-06-19 13:38:35 +0300

    fix logo link

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,8 +1,13 @@
 import React from "react"
 import imgLogo from '../images/logo.png'
+import { Link } from "gatsby"
 
 export function Logo() {
   // с GatsbyImage возникает проблема мерцания логотипа при переходе по страницам
-  return <img src={imgLogo} alt="Заказ воды в Симферополе c бесплатной доставкой" /> 
+  return (
+    <Link to='/'>
+      <img src={imgLogo} alt="Заказ воды в Симферополе c бесплатной доставкой" /> 
+    </Link>
+  ) 
 
 }
