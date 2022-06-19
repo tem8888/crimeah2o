5683aca9f33bf777fd908fcd5b9ed7694d47fcf9
Artem Dmytrishyn <gamegir@gmail.com>
2022-06-19 16:53:48 +0300

    add load more functionality and scroll-to-top button

--- src/pages/kulery.js
+++ src/pages/kulery.js
@@ -33,6 +33,45 @@ export default function Kulery() {
   `)
   const products = data.allMarkdownRemark.nodes
 
+  const [visible, setVisible] = React.useState(false)
+  const [count, setCount] = React.useState(1)
+  const productsPerPage = 12
+
+  React.useEffect(() => {
+    const onScrollChange = () => {
+      if (window.scrollY > 1000) {
+        setVisible(true)
+      } else {
+        setVisible(false)
+      }
+    }
+    document.addEventListener('scroll', onScrollChange)
+    return () => document.removeEventListener('scroll', onScrollChange)
+
+  }, [])
+
+  const scrollUp = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
+
+  const renderShowMoreButton = () => {
+    if (productsPerPage*count < products.length) {
+      return (
+        <div style={{ textAlign: "center", marginBottom: '30px' }}>
+          <button className="button" onClick={() => setCount(count => count + 1)}>Больше кулеров</button>
+        </div>
+      ) 
+    }
+  }
+  const renderScrollTopButton = () => {
+      return (
+        <div className={`arrow-top-btn ${!visible && 'hidden'}`} onClick={scrollUp} />
+      )
+  }
+
   return (
     <Layout>
       <Seo 
@@ -40,7 +79,7 @@ export default function Kulery() {
         description="Компания «Единая доставка воды в Крыму» проиводит доставку кулеров для воды в Симферополе и симферопольском районе." 
         url="/kulery"/>
       <h1 style={{ textAlign: "center" }}>Кулеры для воды</h1>
-      {products.map(product => {
+      {products.slice(0, productsPerPage*count).map(product => {
         return (
           <ProductCard 
             key={product.id} 
@@ -50,6 +89,8 @@ export default function Kulery() {
           />
         ) 
       })}
+      {renderShowMoreButton()}
+      {renderScrollTopButton()}
     </Layout>
   )
 }
